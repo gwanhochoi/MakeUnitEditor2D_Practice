88f5df8be93d1334d0c699b55bb8a8e27e4f0ecf
gwanhochoi <cgh722@gmail.com>
2023-01-16 20:52:32 +0900

    First Commit
    첫 커밋. 대강 UI 모양새만 갖춤

---
+++ MakeUnitEditor2D_Practice/Assets/Scripts/Unit_Script.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit_Script : MonoBehaviour
+{
+    //캐릭터 각 부위 Skin : body, head, leg, arm
+    //각 부위별 아이템도!
+
+    //아이템과 Skin 교체 작업
+
+}
